f7396fd71bf1d6e63f790e50ab8090d56af33d9a
andydangit <andy.dang.it@gmail.com>
2024-09-17 05:42:30 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by FartFire on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
