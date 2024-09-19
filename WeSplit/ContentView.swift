dbbe56401319e6025a7a5bea4ff49b29d515512e
andydangit <andy.dang.it@gmail.com>
2024-09-19 15:52:52 -0500

    Add: Focus toolbar

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -45,6 +46,7 @@ struct ContentView: View {
                 Section{
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople){
                         ForEach(2..<100 ) {
@@ -91,6 +93,13 @@ struct ContentView: View {
             }
             .navigationTitle("Check Split")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
